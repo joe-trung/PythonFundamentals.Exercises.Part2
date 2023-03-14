3436bcf435328189e9132bb2d826769397031f61
joe-trung <kieutrungtran@gmail.com>
2023-03-14 14:34:21 -0400

    greet

---
+++ greet.py
@@ -0,0 +1,11 @@
+def greet(name):
+    print ("Hello " +name)
+greet("Trung Tran")
+greet("Darth Vader")
+greet("Guido van Rossum")
+def name_input():
+    return(input("Please enter your name:"))
+greet(name_input())
+
+
+    
